f00ada907f94827a82e1a0824c91b06513d64ff9
João Victor <padrinhoreserva@gmail.com>
2024-04-24 23:45:19 -0300

    So good so far

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contact(request): 
-    return HttpResponse("Another page in this 'website' :()")
-
-
-def about(request): 
-    return HttpResponse("About section")
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'My name',
+    })
